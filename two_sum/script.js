6207bd68a24fb971ce0f83b152d02de365f40833
Lance Cruzado <raphaellcruzado@gmail.com>
2023-08-14 13:49:54 -0700

    work in progress

--- two_sum/script.js
+++ two_sum/script.js
@@ -1,1 +1,13 @@
-console.log('hello Lance');
+function two_Sum (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        const currentIndex = nums[i];
+        for (let j = 1; j < nums.length; j++) {
+            const testIndex = nums[j];
+            console.log(currentIndex + testIndex);
+        }
+    }
+}
+
+two_Sum([2,7,11,15],9);
+// two_Sum([3,2,4],6);
+// two_Sum([3,3],6);
