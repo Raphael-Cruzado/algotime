ca71b96c27a44f7bc550c78d8210f9f56350fb21
Lance Cruzado <raphaellcruzado@gmail.com>
2023-08-16 00:16:19 -0700

    almost there

--- two_sum/script.js
+++ two_sum/script.js
@@ -1,13 +1,20 @@
-function two_Sum (nums, target) {
+function twoSum (nums, target) {
     for (let i = 0; i < nums.length; i++) {
         const currentIndex = nums[i];
         for (let j = 1; j < nums.length; j++) {
-            const testIndex = nums[j];
-            console.log(currentIndex + testIndex);
+            const addend = nums[j];
+            const sum = currentIndex + addend;
+            if (sum === target && (currentIndex !== addend)) {
+                return([i,j]);
+            }
         }
     }
 }
 
-two_Sum([2,7,11,15],9);
-// two_Sum([3,2,4],6);
-// two_Sum([3,3],6);
+// var two_Sum = twoSum([2,7,11,15],9); // [0,1]
+// var two_Sum = twoSum([3,2,4],6); // [1,2]
+// var two_Sum = twoSum([3,3],6); // [0,1]
+var two_Sum = twoSum([2,5,5,11], 10); // [1,2]
+// ^get testcase above and u gucci
+// just have it so that currentIndex !== addend
+console.log(two_Sum);
