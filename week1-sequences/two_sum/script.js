10356a6c2aa42e89a68c93573d7b837a083c35da
Lance Cruzado <raphaellcruzado@gmail.com>
2023-08-16 20:02:28 -0700

    organizing algos in difficulty

---
+++ week1-sequences/two_sum/script.js
@@ -0,0 +1,18 @@
+function twoSum (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        const currentIndex = nums[i];
+        for (let j = 1; j < nums.length; j++) {
+            const addend = nums[j];
+            const sum = currentIndex + addend;
+            if (sum === target && (i !== j)) {
+                return([i,j]);
+            }
+        }
+    }
+}
+
+// var two_Sum = twoSum([2,7,11,15],9); // [0,1]
+// var two_Sum = twoSum([3,2,4],6); // [1,2]
+// var two_Sum = twoSum([3,3],6); // [0,1]
+var two_Sum = twoSum([2,5,5,11], 10); // [1,2]
+console.log(two_Sum);
