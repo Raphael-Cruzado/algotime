39f0b6d09eae841889d88222e376be0630668f9f
Lance Cruzado <raphaellcruzado@gmail.com>
2023-08-16 21:38:10 -0700

    object oriented programming

--- week1-sequences/contains_duplicate/script.js
+++ week1-sequences/contains_duplicate/script.js
@@ -1,5 +1,11 @@
-function containsDuplicate() {
-    
-}
+let set = new Set(nums);
 
+    if (set.size !== nums.length) {
+        return true
+    } else {
+        return false
+    }
+
+// start playing around with data structures from now on 
+// constructors: Array, Object, Set, Map, Stack, Queue
 
