e914c8232eb0f5be27adc633c168f99dbbb36b8c
Lance Cruzado <raphaellcruzado@gmail.com>
2023-08-17 12:50:26 -0700

    learning contructors

--- week1-sequences/contains_duplicate/script.js
+++ week1-sequences/contains_duplicate/script.js
@@ -1,11 +1,15 @@
-let set = new Set(nums);
+let nums = [1,1,1,3,3,4,3,2,4,2]
 
-    if (set.size !== nums.length) {
-        return true
-    } else {
-        return false
-    }
+var containsDuplicate = function(nums) {
+    let set = new Set(nums);
 
+        if (set.size !== nums.length) {
+            return true
+        } else {
+            return false
+        }
+};
 // start playing around with data structures from now on 
 // constructors: Array, Object, Set, Map, Stack, Queue
 
+console.log(containsDuplicate)
