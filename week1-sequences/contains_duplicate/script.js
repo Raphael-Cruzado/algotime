29451a4f0a04af89d9ab84572e6e1b774a6b8601
Lance Cruzado <raphaellcruzado@gmail.com>
2023-08-17 21:08:25 -0700

    solved

--- week1-sequences/contains_duplicate/script.js
+++ week1-sequences/contains_duplicate/script.js
@@ -1,8 +1,9 @@
-let nums = [1,1,1,3,3,4,3,2,4,2]
 
 var containsDuplicate = function(nums) {
+    debugger
     let set = new Set(nums);
-
+        console.log(set.size);
+        console.log(nums.length);
         if (set.size !== nums.length) {
             return true
         } else {
@@ -12,4 +13,11 @@ var containsDuplicate = function(nums) {
 // start playing around with data structures from now on 
 // constructors: Array, Object, Set, Map, Stack, Queue
 
-console.log(containsDuplicate)
+// In this algo, the Set data structure is being used,
+// the Set ds is useful for eliminating duplicates, if the nums array
+// does not match the sorted Set data structure, it means that there
+// are duplicates because the Set ds sorted out the array to reduce
+// duplicates if it exists
+
+let nums1 = [1,1,1,3,3,4,3,2,4,2];
+console.log(containsDuplicate(nums1));
